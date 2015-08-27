1c00968c1f095ad1442c29167a8d1f92a0fa492a
鲁洋 <luyang@asiainfo.com>
2015-08-27 09:42:10 +0800

    1.获取服务器状态代码优化。2.获取服务器状态，线程池大小修改。

--- src/main/java/com/asiainfo/gim/server/monitor/icmp/ServerStatusCollectJob.java
+++ src/main/java/com/asiainfo/gim/server/monitor/icmp/ServerStatusCollectJob.java
@@ -12,22 +12,21 @@ package com.asiainfo.gim.server.monitor.icmp;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Collection;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import com.asiainfo.gim.common.spring.SpringContext;
 import com.asiainfo.gim.server.Constant;
-import com.asiainfo.gim.server.dao.ServerDao;
 import com.asiainfo.gim.server.domain.Server;
 import com.asiainfo.gim.server.domain.ServerRuntime;
-import com.asiainfo.gim.server.domain.query.ServerQueryCondition;
 
 /**
  * @author luyang
@@ -36,57 +35,64 @@ import com.asiainfo.gim.server.domain.query.ServerQueryCondition;
 public class ServerStatusCollectJob implements Job
 {
 	private static Log log = LogFactory.getLog(ServerStatusCollectJob.class);
-	
+
 	public void execute(JobExecutionContext context) throws JobExecutionException
 	{
 		CacheManager cacheManager = (CacheManager) SpringContext.getBean("cacheManager");
-		ServerDao serverDao = SpringContext.getBean(ServerDao.class);
-		
-		Cache cache = cacheManager.getCache(Constant.CacheName.SERVER_CACHE);
 		ThreadPoolTaskExecutor thread = (ThreadPoolTaskExecutor) SpringContext.getBean("taskExecutThreadPool");
-		
-		for (Server server : serverDao.listServers(new ServerQueryCondition()))
+
+		Collection<Object> servers = ((Map)cacheManager.getCache(Constant.CacheName.SERVER_CACHE).getNativeCache()).values();
+		for (Object obj : servers)
 		{
-			Server serverInCache = (Server) cache.get(server.getId()).get();
+			
+			Server serverInCache = (Server) obj;
 			thread.execute(new CollectThread(serverInCache));
-			cache.put(server.getId(), serverInCache);
 		}
 	}
 
 	private class CollectThread implements Runnable
 	{
 		private Server server;
-		
+
 		public CollectThread(Server server2)
 		{
 			this.server = server2;
 		}
-		
+
 		public void run()
 		{
-			try {
-	            InetAddress address = InetAddress.getByName(server.getIp());
-	            ServerRuntime serverRuntime = new ServerRuntime();
-	            if (server.getServerRuntime() != null)
+			ServerRuntime serverRuntime = server.getServerRuntime();
+			if (server.getServerRuntime() == null)
+			{
+				serverRuntime = new ServerRuntime();
+				server.setServerRuntime(serverRuntime);
+			}
+
+			try
+			{
+				InetAddress address = InetAddress.getByName(server.getIp());
+
+				// 1表示状态OK，0表示状态异常
+				if (address.isReachable(3000))
 				{
-	            	serverRuntime = server.getServerRuntime();
+					serverRuntime.setStatus(1);
 				}
-	            //1表示状态OK，0表示状态异常
-	            if (address.isReachable(5000))
+				else
 				{
-	            	serverRuntime.setStatus(1);
+					serverRuntime.setStatus(0);
 				}
-	            else
-	            {
-	            	serverRuntime.setStatus(0);
-	            }
-	            server.setServerRuntime(serverRuntime);
-	        } catch (UnknownHostException e) {
-	            log.error(e.getMessage(), e);
-	        } catch (IOException e) {
-	            log.error(e.getMessage(), e);
-	        }
+			}
+			catch (UnknownHostException e)
+			{
+				serverRuntime.setStatus(0);
+				log.error(e.getMessage(), e);
+			}
+			catch (IOException e)
+			{
+				serverRuntime.setStatus(0);
+				log.error(e.getMessage(), e);
+			}
 		}
-		
+
 	}
 }
